c6ab3a5ac8b07b66404668ce1e603db477df9541
Frankelyn <Frankelyn_garcia@ucne.edu.do>
2022-06-25 22:30:07 -0400

    Colocando Screen en el MainActivity

--- app/src/main/java/com/ejemplo/coinswapi/MainActivity.kt
+++ app/src/main/java/com/ejemplo/coinswapi/MainActivity.kt
@@ -10,8 +10,11 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.ejemplo.coinswapi.ui.Screens.CoinsScreen
 import com.ejemplo.coinswapi.ui.theme.CoinsWApiTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,22 +25,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                   CoinsScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CoinsWApiTheme {
-        Greeting("Android")
     }
 }
