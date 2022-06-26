1b6272d6048d4dda3b6d654cd8b74d0fcad666bc
Frankelyn <Frankelyn_garcia@ucne.edu.do>
2022-06-25 21:43:26 -0400

    Creando AppModule y utilizando Moshi

---
+++ app/src/main/java/com/ejemplo/coinswapi/di/AppModule.kt
@@ -0,0 +1,34 @@
+package com.ejemplo.coinswapi.di
+
+import com.ejemplo.coinswapi.data.remote.API.CoinsAPI
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi {
+        return Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideCoinApi(moshi: Moshi): CoinsAPI {
+        return Retrofit.Builder()
+            .baseUrl("http://www.yamato07.somee.com")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(CoinsAPI::class.java)
+    }
+}
