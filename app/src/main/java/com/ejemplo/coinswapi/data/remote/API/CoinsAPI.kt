10e9ece679e33a029665974fb3dd23c4aa458ef3
Frankelyn <Frankelyn_garcia@ucne.edu.do>
2022-07-01 11:27:15 -0400

    Creando metodo Post en la CoinsApi

--- app/src/main/java/com/ejemplo/coinswapi/data/remote/API/CoinsAPI.kt
+++ app/src/main/java/com/ejemplo/coinswapi/data/remote/API/CoinsAPI.kt
@@ -1,9 +1,15 @@
 package com.ejemplo.coinswapi.data.remote.API
 
 import com.ejemplo.coinswapi.data.remote.Dto.CoinDto
+import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface CoinsAPI {
     @GET("/Coins")
     suspend fun getCoins() : List<CoinDto>
+
+    @POST("/Coins")
+    suspend fun setCoins(@Body coinDto: CoinDto): Response<CoinDto>
 }
--- app/src/main/java/com/ejemplo/coinswapi/ui/Screens/RegistroCoinsScreen.kt
+++ app/src/main/java/com/ejemplo/coinswapi/ui/Screens/RegistroCoinsScreen.kt
@@ -3,14 +3,17 @@ package com.ejemplo.coinswapi.ui.Screens
 import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Save
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -23,6 +26,13 @@ fun Toolbar2() {
 @Preview(showBackground = true)
 @Composable
 fun RegistroCoinScreen(){
+
+   /* var descripcionError by remember { mutableStateOf(false) }
+    var valorError by remember { mutableStateOf(false) }
+    val context = LocalContext.current
+    val scaffoldState = rememberScaffoldState()
+
+    var Texto: String = " "
     Scaffold(
         topBar = { Toolbar2() }
     ) {
@@ -32,8 +42,40 @@ fun RegistroCoinScreen(){
                 .padding(it)
                 .padding(8.dp)
         ) {
+            OutlinedTextField(
+               // value = ,
+               // onValueChange = { viewModel.txDeudor = it
+                   // deudorError = false},
+                modifier = Modifier.fillMaxWidth(),
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Person,
+                        contentDescription = null
+                    )
+                },
+                isError = descripcionError
+            )
 
+           // OutlinedTextField(value = Texto, onValueChange = Texto )
 
         }
+    }*/
+}
+
+
+@Composable
+fun validar(error: Boolean){
+    val assistiveElementText = if(error) "Error: Obligatorio" else "*obligatorio"
+    val assistiveElementColor = if(error) {
+        MaterialTheme.colors.error
+    }else{
+        MaterialTheme.colors.onSurface.copy(alpha = ContentAlpha.medium)
     }
+
+    Text(
+        text = assistiveElementText,
+        color = assistiveElementColor,
+        style = MaterialTheme.typography.caption,
+        modifier = Modifier.padding(start = 16.dp)
+    )
 }
