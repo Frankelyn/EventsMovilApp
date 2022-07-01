343ca9d30514c67e9e71478fdead3f4818686d2f
Frankelyn <Frankelyn_garcia@ucne.edu.do>
2022-07-01 11:44:58 -0400

    Creando metodo setCoin al viewmodel

--- app/src/main/java/com/ejemplo/coinswapi/ViewModels/CoinsViewModel.kt
+++ app/src/main/java/com/ejemplo/coinswapi/ViewModels/CoinsViewModel.kt
@@ -1,20 +1,26 @@
 package com.ejemplo.coinswapi.ViewModels
 
 import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ejemplo.coinswapi.data.Repositories.CoinsDtoRepository
+import com.ejemplo.coinswapi.data.remote.Dto.CoinDto
 import com.ejemplo.coinswapi.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class CoinsViewModel @Inject constructor(
     private val coinsDtoRepository: CoinsDtoRepository
 ) : ViewModel() {
+
+    val txnombreCoin by mutableStateOf("")
+    val txprecio by mutableStateOf("")
 
     private var _state = mutableStateOf(CoinsState())
     val state: State<CoinsState> = _state
@@ -36,5 +42,15 @@ class CoinsViewModel @Inject constructor(
         }.launchIn(viewModelScope)
     }
 
+    fun setCoin() {
+        viewModelScope.launch {
+            coinsDtoRepository.setCoins(
+                CoinDto(
+                    descripcion = txnombreCoin,
+                    valor = txprecio.toDouble()
+                )
+            )
+        }
+    }
     
 }
